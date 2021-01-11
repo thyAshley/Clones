073fd12dff29e619d4b6e2cb0cdc32cb47b87241
Ashley <thy.ashley@gmail.com>
2021-01-11 14:51:58 +0800

    added scss

--- linkedin-clone/src/components/Sidebar/Sidebar.tsx
+++ linkedin-clone/src/components/Sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="sidebar__top">
         <img
+          className="sidebar__top__bg"
           src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
           alt="background"
         />
@@ -16,11 +17,11 @@ const Sidebar = () => {
       </div>
 
       <div className="sidebar__stats">
-        <div className="sidebar_-stat">
+        <div className="sidebar__stat">
           <p>Who viewed you</p>
           <p className="sidebar__statNumber">1,234</p>
         </div>
-        <div className="sidebar_-stat">
+        <div className="sidebar__stat">
           <p>Views on post</p>
           <p className="sidebar__statNumber">1,010</p>
         </div>
