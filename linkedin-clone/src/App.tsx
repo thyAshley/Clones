f47b0b87622d80a49c0f284477f78ed7122ee424
Ashley <thy.ashley@gmail.com>
2021-01-11 14:12:30 +0800

    fix images

--- linkedin-clone/src/App.tsx
+++ linkedin-clone/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { Header, Sidebar } from "./components";
 
 function App() {
--- linkedin-clone/src/components/Sidebar/Sidebar.tsx
+++ linkedin-clone/src/components/Sidebar/Sidebar.tsx
@@ -6,12 +6,29 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHw%3D&w=1000&q=80"
+          alt="background"
+        />
         <Avatar className="sidebar__avatar" src="/images/avatar.png" />
         <h2>Lorem, ipsum.</h2>
         <h4>lorem@gmail.com</h4>
       </div>
 
-      <div className="sidebar__stats"></div>
+      <div className="sidebar__stats">
+        <div className="sidebar_-stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">1,234</p>
+        </div>
+        <div className="sidebar_-stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">1,010</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
     </div>
   );
 };
